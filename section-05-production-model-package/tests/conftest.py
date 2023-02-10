d6b2bcf4711f2c4593ad997ccf454ff348b0278d
sephcodes <yosephkawad@gmail.com>
2023-02-10 15:09:27 -0500

    committing changes

--- section-05-production-model-package/tests/conftest.py
+++ section-05-production-model-package/tests/conftest.py
@@ -3,7 +3,8 @@ import pytest
 from regression_model.config.core import config
 from regression_model.processing.data_manager import load_dataset
 
-
+#this fixture gets called by the other test_ methods
+#tox e- test_package runs the tests in /tests folder
 @pytest.fixture()
 def sample_input_data():
     return load_dataset(file_name=config.app_config.test_data_file)
